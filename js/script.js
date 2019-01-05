d300aad66acb9ed58df9aaa191a5d29b5b7ba6e7
Justin Fan <jjustinfan1997@gmail.com>
2019-01-05 18:42:05 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 5000, 
+        pagination: false
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Student", "Web Developer", "Software Engineer", "Project Manager"],
+        typeSpeed: 40,
+        loop: true, 
+        startDelay: 1000
+    })
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items:4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    })
+    
+
+    
+    var skillsTopOffset = $('.skillsSection').offset().top;
+    var statsTopOffset = $('.statsSection').offset().top;
+    var countUpFinished = false;
+
+    $(window).scroll(function() {  
+        if(this.window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+            $('.chart').easyPieChart({
+                //your options goes here
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4, 
+                size: 152,
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
+        if(!countUpFinished && this.window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+            $('.counter').each(function() {
+                var element = $(this);
+                var endVal = parseInt(element.text());
+                element.countup(endVal)
+            })
+            countUpFinished = true
+        }
+    })
+
+    $("[data-fancybox]").fancybox()
+
+    $(".items").isotope({
+        filter:"*", 
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue:false
+        }
+    })
+
+    $("#filters a").click(function() {
+        $("#filters .current").removeClass("current")
+        $(this).addClass("current");
+        
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector, 
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue:false
+            }
+        })
+        return false;
+    })
+
+
+});
+
